27e5a661e6aa4ae15cc6887d6693932dad0852ae
Murilo Vargas <muriraubach@gmail.com>
2023-06-06 15:51:53 -0300

    add dto validations

--- src/modules/cargoType/dtos/CreatePerishableCargoTypeDTO.java
+++ src/modules/cargoType/dtos/CreatePerishableCargoTypeDTO.java
@@ -1,8 +1,14 @@
 package modules.cargoType.dtos;
 
+import javax.validation.constraints.NotBlank;
+
 public class CreatePerishableCargoTypeDTO {
     private String description;
+
+    @NotBlank(message = "The origin cannot be blank")
     private String origin;
+
+    @NotBlank(message = "The max validity time cannot be blank")
     private int maxValidityTime;
 
     public CreatePerishableCargoTypeDTO(String description, String origin,
