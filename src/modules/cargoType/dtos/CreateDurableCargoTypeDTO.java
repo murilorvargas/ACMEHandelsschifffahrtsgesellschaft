cd0191d393029e3bddc55e78dafa1b2dba54452c
Murilo Vargas <muriraubach@gmail.com>
2023-06-06 15:46:32 -0300

    add create durable cargo type dto validation

--- src/modules/cargoType/dtos/CreateDurableCargoTypeDTO.java
+++ src/modules/cargoType/dtos/CreateDurableCargoTypeDTO.java
@@ -1,24 +1,27 @@
 package modules.cargoType.dtos;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
 public class CreateDurableCargoTypeDTO {
-    private String number;
     private String description;
+
+    @NotBlank(message = "The sector cannot be blank")
     private String sector;
+
+    @NotBlank(message = "The main material cannot be blank")
     private String mainMaterial;
+
+    @Positive(message = "The IPI percentage must be a positive value")
     private double ipiPercentage;
 
-    public CreateDurableCargoTypeDTO(String number, String description, String sector, String mainMaterial,
+    public CreateDurableCargoTypeDTO(String description, String sector, String mainMaterial,
             double ipiPercentage) {
-        this.number = number;
         this.description = description;
         this.sector = sector;
         this.mainMaterial = mainMaterial;
         this.ipiPercentage = ipiPercentage;
 
-    }
-
-    public String getNumber() {
-        return number;
     }
 
     public String getDescription() {
