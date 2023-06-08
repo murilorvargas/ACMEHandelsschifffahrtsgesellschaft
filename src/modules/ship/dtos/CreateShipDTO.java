5739e088276a85400c99e3bd7b252c4f47a11241
Leonardo <leonardosiqueira387@gmail.com>
2023-06-08 19:19:33 -0300

    add create ship feature DTO

---
+++ src/modules/ship/dtos/CreateShipDTO.java
@@ -0,0 +1,55 @@
+package modules.ship.dtos;
+
+import java.util.UUID;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+
+public class CreateShipDTO {
+
+    @NotBlank(message = "The id cannot be blank.")
+    private UUID id;
+
+    @NotBlank(message = "The name cannot be blank.")
+    private String name;
+
+    @NotBlank(message = "The speed cannot be blank.")
+    @PositiveOrZero(message = "The speed must be a positive value.")
+    private double speed;
+
+    @NotBlank(message = "The autonomy cannot be blank.")
+    @PositiveOrZero(message = "The autonomy must be a positive value.")
+    private double autonomy;
+
+    @NotBlank(message = "The cost per mile cannot be blank.")
+    @PositiveOrZero(message = "The cost per mile must be a positive value.")
+    private double costPerMile;
+
+    public CreateShipDTO(String name, double speed, double autonomy, double costPerMile) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.speed = speed;
+        this.autonomy = autonomy;
+        this.costPerMile = costPerMile;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getAutonomy() {
+        return autonomy;
+    }
+
+    public double getCostPerMile() {
+        return costPerMile;
+    }
+
+}
