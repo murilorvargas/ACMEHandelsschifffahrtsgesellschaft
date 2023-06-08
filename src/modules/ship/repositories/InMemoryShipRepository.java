7fdc8fd33e3c4695df983956227a864baa637f4d
Leonardo <leonardosiqueira387@gmail.com>
2023-06-08 19:21:17 -0300

    add ship In memory repository basic logic' '

---
+++ src/modules/ship/repositories/InMemoryShipRepository.java
@@ -0,0 +1,53 @@
+package modules.ship.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import modules.ship.entities.Ship;
+import modules.ship.entities.interfaces.IShipReadable;
+import modules.ship.repositories.interfaces.IShipRepository;
+
+public class InMemoryShipRepository implements IShipRepository {
+
+    private List<Ship> shipList;
+    private static InMemoryShipRepository instance = null;
+
+    public static synchronized InMemoryShipRepository instanceOf() {
+        if (instance == null) {
+            instance = new InMemoryShipRepository();
+        }
+        return instance;
+    }
+
+    private InMemoryShipRepository() {
+        this.shipList = new ArrayList<Ship>();
+    }
+
+    @Override
+    public IShipReadable create(String name, double speed, double autonomy, double costPerMile) {
+        Ship ship = new Ship(name, speed, autonomy, costPerMile);
+        this.shipList.add(ship);
+
+        return ship;
+    }
+
+    @Override
+    public List<IShipReadable> findAll() {
+        List<IShipReadable> shipSet = new ArrayList<>();
+        for (Ship ship : this.shipList) {
+            shipSet.add(ship);
+        }
+        return shipSet;
+    }
+
+    @Override
+    public IShipReadable findByName(String name) {
+        for (Ship ship : this.shipList) {
+            if (ship.getName().equals(name)) {
+                return ship;
+            }
+        }
+        return null;
+    }
+
+}
