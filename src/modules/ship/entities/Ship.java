ef255d1aa3914c0a47fe0deb196d08a3c8555d50
Leonardo <leonardosiqueira387@gmail.com>
2023-06-08 19:20:48 -0300

    add ship basic class

---
+++ src/modules/ship/entities/Ship.java
@@ -0,0 +1,47 @@
+package modules.ship.entities;
+
+import java.util.UUID;
+import modules.ship.entities.interfaces.IShipReadable;
+
+public class Ship implements Comparable<Ship>, IShipReadable {
+
+    private UUID id;
+    private String name;
+    private double speed;
+    private double autonomy;
+    private double costPerMile;
+
+    public Ship(String name, double speed, double autonomy, double costPerMile) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.speed = speed;
+        this.autonomy = autonomy;
+        this.costPerMile = costPerMile;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getAutonomy() {
+        return autonomy;
+    }
+
+    public double getCostPerMile() {
+        return costPerMile;
+    }
+
+    @Override
+    public int compareTo(Ship other) {
+        return this.name.compareTo(other.name);
+    }
+
+}
