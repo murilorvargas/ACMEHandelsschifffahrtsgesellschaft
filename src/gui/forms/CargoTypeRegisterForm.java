db0648b19300f8b657eb0b165fa9765e1a608d44
Luiz Krug <luizg.krug@gmail.com>
2023-06-20 20:15:41 -0300

    add cargotype

--- src/gui/forms/CargoTypeRegisterForm.java
+++ src/gui/forms/CargoTypeRegisterForm.java
@@ -34,6 +34,8 @@ public class CargoTypeRegisterForm extends JFrame {
     private String setor;
     private String material;
     private double porcentagemIPI;
+    private boolean perecivel = false;
+    private boolean duravel = false;
     private CargoTypeController cargoTypeController;
 
     public CargoTypeRegisterForm() {
@@ -90,8 +92,14 @@ public class CargoTypeRegisterForm extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 campoNumero.setEnabled(true);
                 campoDescricao.setEnabled(true);
+
                 campoOrigem.setEnabled(true);
+                campoOrigem.setVisible(true);
+                rotuloOrigem.setVisible(true);
+
                 campoValidadeMaxima.setEnabled(true);
+                campoValidadeMaxima.setVisible(true);
+                rotuloValidadeMaxima.setVisible(true);
 
                 campoSetor.setEnabled(false);
                 campoSetor.setVisible(false);
@@ -110,14 +118,98 @@ public class CargoTypeRegisterForm extends JFrame {
             }
         });
 
+        botaoDuravel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                campoNumero.setEnabled(true);
+                campoDescricao.setEnabled(true);
+
+                campoSetor.setEnabled(true);
+                campoSetor.setVisible(true);
+                rotuloSetor.setVisible(true);
+
+                campoMaterial.setEnabled(true);
+                campoMaterial.setVisible(true);
+                rotuloMaterial.setVisible(true);
+
+                campoPorcentagemIPI.setEnabled(true);
+                campoPorcentagemIPI.setVisible(true);
+                rotuloPorcentagemIPI.setVisible(true);
+
+                campoOrigem.setEnabled(false);
+                campoOrigem.setVisible(false);
+                rotuloOrigem.setVisible(false);
+
+                campoValidadeMaxima.setEnabled(false);
+                campoValidadeMaxima.setVisible(false);
+                rotuloValidadeMaxima.setVisible(false);
+
+
+
+                campoPorcentagemIPI.setEnabled(false);
+                campoPorcentagemIPI.setVisible(false);
+                rotuloPorcentagemIPI.setVisible(false);
+
+                mensagem.setText("Duravel selecionado. Preencha os campos obrigatórios.");
+
+            }
+        });
+
         // Tratamento de evento do botão cadastrar
         botaoCadastrar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                numero = Integer.parseInt(campoNumero.getText());
-                descricao = campoDescricao.getText();
-                origem = campoOrigem.getText();
-                validadeMaxima = Integer.parseInt(campoValidadeMaxima.getText());
+                if(!perecivel && !duravel){
+                    mensagem.setText("Selecione um tipo de carga.");
+                    return;
+                }
+
+                if(perecivel) {
+                    if (campoNumero.getText().equals("") || campoDescricao.getText().equals("") || campoOrigem.getText().equals("") || campoValidadeMaxima.getText().equals("")) {
+                        mensagem.setText("Preencha todos os campos obrigatórios.");
+                        return;
+                    }
+                    try {
+                        numero = Integer.parseInt(campoNumero.getText());
+                    } catch (NumberFormatException ex){
+                        mensagem.setText("Número inválido.");
+                        return;
+                    }
+                    try {
+                        validadeMaxima = Integer.parseInt(campoValidadeMaxima.getText());
+                    } catch (NumberFormatException ex){
+                        mensagem.setText("Validade máxima inválida.");
+                        return;
+                    }
+                    descricao = campoDescricao.getText();
+                    origem = campoOrigem.getText();
+                    perishableCargoRegister();
+
+                }
+                if(duravel) {
+                    if (campoNumero.getText().equals("") || campoDescricao.getText().equals("") || campoSetor.getText().equals("") || campoMaterial.getText().equals("") || campoPorcentagemIPI.getText().equals("")) {
+                        mensagem.setText("Preencha todos os campos obrigatórios.");
+                        return;
+                    }
+                    try {
+                        numero = Integer.parseInt(campoNumero.getText());
+                    } catch (NumberFormatException ex){
+                        mensagem.setText("Número inválido.");
+                        return;
+                    }
+                    try {
+                        porcentagemIPI = Double.parseDouble(campoPorcentagemIPI.getText());
+                    } catch (NumberFormatException ex){
+                        mensagem.setText("Porcentagem IPI inválida.");
+                        return;
+                    }
+                    descricao = campoDescricao.getText();
+                    setor = campoSetor.getText();
+                    material = campoMaterial.getText();
+                    durableCargoRegister();
+
+                }
+                
 
             }
         });
@@ -173,18 +265,32 @@ public class CargoTypeRegisterForm extends JFrame {
         CargoTypeRegisterForm janela = new CargoTypeRegisterForm();
     }
 
-    // private void cargoTypeRegister() {
-    // try {
-    // cargoTypeController.onCreateCargoType(numero, descricao, origem,
-    // validadeMaxima);
-    // } catch (BaseRunTimeException e) {
-    // message.setText(e.getTranslation());
-    // return;
-    // } catch (Exception e) {
-    // message.setText("Erro ao ler o arquivo.");
-    // return;
-    // }
-    // new RegisterMenu();
-    // setVisible(false);
-    // }
+    private void perishableCargoRegister() {
+        try {
+            cargoTypeController.onCreatePerishableCargoType(numero, descricao, origem,
+            validadeMaxima);
+        } catch (BaseRunTimeException e) {
+            mensagem.setText(e.getTranslation());
+            return;
+        } catch (Exception e) {
+            mensagem.setText("Erro ao ler o arquivo.");
+            return;
+        }
+        new RegisterMenu();
+        setVisible(false);
+    }
+
+     private void durableCargoRegister() {
+        try {
+            cargoTypeController.onCreateDurableCargoType(numero, descricao, setor, material, porcentagemIPI);
+        } catch (BaseRunTimeException e) {
+            mensagem.setText(e.getTranslation());
+            return;
+        } catch (Exception e) {
+            mensagem.setText("Erro ao ler o arquivo.");
+            return;
+        }
+        new RegisterMenu();
+        setVisible(false);
+    }
 }
